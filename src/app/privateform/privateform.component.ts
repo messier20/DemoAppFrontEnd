059f3599396ea87462638de455adb413de6ee4dd
Ignas <ignasbobinas@gmail.com>
2018-03-20 23:20:13 +0200

    added lists of car brands and models

--- src/app/privateform/privateform.component.ts
+++ src/app/privateform/privateform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CarList} from '../models/CarList';
 
 @Component({
   selector: 'app-privateform',
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./privateform.component.css']
 })
 export class PrivateformComponent implements OnInit {
+  
   customerType;
   assetType;
   carBrand;
   carModel;
   manufacturedDate;
-
   enginePower;
   assetPrice;
   advancePaymentPercentage;
@@ -23,16 +24,32 @@ export class PrivateformComponent implements OnInit {
 
   availableCustomerTypes = ['Private', 'Business'];
   availableAssetTypes = ['Vehicle'];
+  cars;
+  model: String[];
   availableDays = [15, 30];
 
-  constructor() { }
+  constructor() {
+    this.cars = new CarList().cars;
+   }
 
   ngOnInit() {
   }
-
+  
+  selectBrandHandler( event: any){
+    for (let i = 0; i < this.cars.length; i++) {
+      if(this.cars[i].make === this.carBrand){
+        this.model = this.cars[i].model;
+        // document.getElementById("carModel").hidden = false;
+        break;
+      }
+    }
+  }
+  
   submitForm() {
     console.log('Customer type: ' + this.customerType);
-    console.log('Asset type' + this.assetType);
+    console.log('Asset type: ' + this.assetType);
+    console.log('Car make: ' + this.carBrand);
+    console.log('Car model: ' + this.carModel);
     console.log('Engine power: ' + this.enginePower);
     console.log('Asset price: ' + this.assetPrice);
     console.log('Advance payment percentage: ' + this.advancePaymentPercentage);
