74512e2e0f3e9ad2d649b028dc22144c432b0fa0
DonatasT <donatas.tamosauskas@gmail.com>
2018-03-20 18:02:36 +0200

    Asset price - payment day input fields added.

--- src/app/privateform/privateform.component.ts
+++ src/app/privateform/privateform.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivateformComponent implements OnInit {
 
+  customerType;
+  assetType;
+  carBrand;
+  carModel;
+  manufacturedDate;
+  enginePower;
+
+  assetPrice;
+  advancePaymentPercentage;
+  advancePaymentAmount;
+  leasePeriodInMonths;
+  margin;
+  contractFee;
+  paymentDate;
+
+  availableDays = [1, 30];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+    console.log('Asset price: ' + this.assetPrice);
+    console.log('Advance payment percentage: ' + this.advancePaymentPercentage);
+    console.log('Lease period in months: ' + this.leasePeriodInMonths);
+    console.log('Margin: ' + this.margin);
+    console.log('Payment date: ' + this.paymentDate);
   }
 
 }
