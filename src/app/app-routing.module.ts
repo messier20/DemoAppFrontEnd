654fd7cd1ecede48ea126d4c13117bb793911f44
Sleper <saliamonasalgirdas@gmail.com>
2018-03-21 08:57:37 -0700

    Private and business forms after submition.needs routing logic.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrivateformComponent } from './privateform/privateform.component';
+import { PrivateCustomerPersonalFormComponent } from './private-customer-personal-form/private-customer-personal-form.component';
+import {BusinessCustomerInfo} from './models/businessCustomerInfo';
+import {BusinessCustomerPersonalFormComponent} from './business-customer-personal-form/business-customer-personal-form.component';
 import {DisplayFormComponent} from './display-form/display-form.component';
 
 const routes: Routes = [
-  {path: 'privateForm', component: PrivateformComponent},
+  {path: 'privateForm', component: PrivateformComponent}
+  {path: 'privatePersonalForm', component: PrivateCustomerPersonalFormComponent},
+  {path: 'businessPersonalForm', component: BusinessCustomerPersonalFormComponent}
   {path: 'displayForm', component: DisplayFormComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PrivateCustomerLeasingFormComponent } from './private-customer-leasing-form/private-customer-leasing-form.component';
-
+// import { ServicesComponent } from './services/services.component';
 import { PrivateformComponent } from './privateform/privateform.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material';
 import {DialogFormComponent, DisplayFormComponent} from './display-form/display-form.component';
-
-
+import {PrivateCustomerPersonalFormComponent} from './private-customer-personal-form/private-customer-personal-form.component';
+import {BusinessCustomerPersonalFormComponent} from './business-customer-personal-form/business-customer-personal-form.component';
 import { DataStorageService } from './services/data-storage-service.service';
 
 
@@ -20,7 +21,11 @@ import { DataStorageService } from './services/data-storage-service.service';
   declarations: [
     AppComponent,
     PrivateCustomerLeasingFormComponent,
+    // ServicesComponent,
     PrivateformComponent,
+    // ReactiveFormsModule
+    PrivateCustomerPersonalFormComponent,
+    BusinessCustomerPersonalFormComponent,
     DisplayFormComponent,
     DialogFormComponent
   ],
