9fb72f4fcfd37962b9baa6647ec18bf5227fbbb4
Ignas <ignasbobinas@gmail.com>
2018-03-21 12:15:09 +0200

    updated manufacturedDate and leasingPeriod fields

--- src/app/privateform/privateform.component.ts
+++ src/app/privateform/privateform.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CarList} from '../models/CarList';
 
 @Component({
@@ -7,19 +7,19 @@ import {CarList} from '../models/CarList';
   styleUrls: ['./privateform.component.css']
 })
 export class PrivateformComponent implements OnInit {
-  
+
   customerType;
   assetType;
   carBrand;
   carModel;
   manufacturedDate;
   enginePower;
-  assetPrice:number;
-  advancePaymentPercentage:number;
-  advancePaymentAmount:number;
+  assetPrice: number;
+  advancePaymentPercentage: number;
+  advancePaymentAmount: number;
   leasePeriodInMonths;
   margin;
-  contractFee:number;
+  contractFee: number;
   paymentDate;
 
   availableCustomerTypes = ['Private', 'Business'];
@@ -28,32 +28,33 @@ export class PrivateformComponent implements OnInit {
   model: String[];
   availableDays = [15, 30];
 
+
   constructor() {
     this.cars = new CarList().cars;
-   }
+  }
 
   ngOnInit() {
   }
-  
-  selectBrandHandler( event: any){
+
+  selectBrandHandler() {
     for (let i = 0; i < this.cars.length; i++) {
-      if(this.cars[i].make === this.carBrand){
+      if (this.cars[i].make === this.carBrand) {
         this.model = this.cars[i].model;
         // document.getElementById("carModel").hidden = false;
         break;
       }
     }
   }
-  
-  calcContractFee(event:any){
-    this.contractFee = this.assetPrice*0.01;
-    if(this.contractFee<200){
+
+  calcContractFee() {
+    this.contractFee = this.assetPrice * 0.01;
+    if (this.contractFee < 200) {
       this.contractFee = 200;
     }
   }
 
-  calcAdvancePaymentAmount(event:any){
-      this.advancePaymentAmount = this.assetPrice*this.advancePaymentPercentage/100;
+  calcAdvancePaymentAmount() {
+    this.advancePaymentAmount = this.assetPrice * this.advancePaymentPercentage / 100;
   }
 
   submitForm() {
@@ -61,11 +62,14 @@ export class PrivateformComponent implements OnInit {
     console.log('Asset type: ' + this.assetType);
     console.log('Car make: ' + this.carBrand);
     console.log('Car model: ' + this.carModel);
+    console.log('Manufactured date: ' + this.manufacturedDate);
     console.log('Engine power: ' + this.enginePower);
     console.log('Asset price: ' + this.assetPrice);
     console.log('Advance payment percentage: ' + this.advancePaymentPercentage);
+    console.log('Advance payment amount: ' + this.advancePaymentAmount);
     console.log('Lease period in months: ' + this.leasePeriodInMonths);
     console.log('Margin: ' + this.margin);
+    console.log('Contract fee: ' + this.contractFee);
     console.log('Payment date: ' + this.paymentDate);
   }
 
