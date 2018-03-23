344ebd6b25178e6a6ff984e708638469768e0739
Ignas <ignasbobinas@gmail.com>
2018-03-23 16:17:49 +0200

    finally removed customer leasing form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { PrivateCustomerLeasingFormComponent } from './private-customer-leasing-form/private-customer-leasing-form.component';
-import { PrivateformComponent } from './privateform/privateform.component';
+import {AppComponent} from './app.component';
+import {PrivateformComponent} from './privateform/privateform.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { DataStorageService } from './services/data-storage-service.service';
+import {DataStorageService} from './services/data-storage-service.service';
 import {BusinessCustomerPersonalFormComponent} from './business-customer-personal-form/business-customer-personal-form.component';
 import {PrivateCustomerPersonalFormComponent} from './private-customer-personal-form/private-customer-personal-form.component';
 import {DisplayFormComponent} from './display-form/display-form.component';
@@ -20,7 +19,6 @@ import {DialogFormComponent} from './display-form/dialog-form/dialog-form';
 @NgModule({
   declarations: [
     AppComponent,
-    PrivateCustomerLeasingFormComponent,
     PrivateformComponent,
     DisplayFormComponent,
     PrivateCustomerPersonalFormComponent,
@@ -40,4 +38,5 @@ import {DialogFormComponent} from './display-form/dialog-form/dialog-form';
   bootstrap: [AppComponent],
   entryComponents: [DisplayFormComponent, DialogFormComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
