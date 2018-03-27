e96083d6b9f1a0d41b62c83b4c3d6f177a3d11c2
monikossite <monikossite@gmail.com>
2018-03-27 22:58:43 +0300

    set leasing model in officer view

--- src/app/officer-view/officer-view.component.ts
+++ src/app/officer-view/officer-view.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {DataStorageService} from "../services/data-storage-service.service";
 import {BackendService} from "../services/backend.service";
+import {LeasingModel} from "../models/LeasingModel";
+import {BusinessCustomerInfo} from "../models/businessCustomerInfo";
+import {PrivateCustomerInfo} from "../models/privateCustomerInfo";
 
 @Component({
   selector: 'app-officer-view',
@@ -10,11 +13,16 @@ import {BackendService} from "../services/backend.service";
 })
 export class OfficerViewComponent implements OnInit {
 
+   leasesModel: [LeasingModel];
+  private businessCustomerModel: BusinessCustomerInfo;
+  private privateCustomerModel: PrivateCustomerInfo;
   public isCollapsed = false;
   public ob;
   leases;
 
-  constructor(private dataService: DataStorageService, private backendService: BackendService) {
+
+  constructor(private dataService: DataStorageService,
+              private backendService: BackendService,) {
 
   }
 
@@ -24,16 +32,62 @@ export class OfficerViewComponent implements OnInit {
     //   // this.dataService.getLeasingModel(),
     // ];
     this.refresh();
+    this.setLeases();
   }
-
 
 
   refresh() {
+
+    // console.log("lalala ", this.leasesModel.carModel)
     this.backendService.getAllPosts()
       .then(data => {
         this.leases = data;
-        console.log('subscribe', this.leases)
+        this.leases.forEach(lease => {
+          // this.leasesModel.customerType = lease.customerLeasingForm.customerType;
+          // console.log("lease model: ", this.leasesModel.customerType);
+          // this.leasesModel.carModel = lease.customerLeasingForm.carModel;
+          this.dataService.setLeasingModel(lease.customerLeasingForm);
+          this.leasesModel = [this.dataService.getLeasingModel()];
+          console.log("lease: ", lease.customerLeasingForm.carModel);
+          console.log("set", this.dataService.getLeasingModel().carModel);
+          console.log("this leases model: ", this.leasesModel);
+        })
+
+        // console.log('subscribe', this.leases)
+        // console.log("cust type", this.leasesModel.customerType);
       });
+
+
+    // return this.leasesModel;
+
+    // return this.leases;
   }
 
+  setLeases() {
+    // this.leases.forEach(lease => {
+    //   console.log("lease sete", lease);
+    //     this.leasesModel = this.dataService.getLeasingModel();
+    //     console.log("leasesmodel", this.dataService.getLeasingModel());
+    //   });
+    // console.log("getas", this.dataService.getLeasingModel());
+    // console.log("getas2", this.leasesModel);
+      //   this.leases.forEach(lease => {
+      //   this.leasesModel = this.dataService.getLeasingModel();
+      //   console.log("leasesmodel", this.dataService.getLeasingModel());
+      // }));
+
+
+    //
+    // setLeases() {
+    //   this.refresh().forEach(data => {
+    //     console.log("test", data);
+    //   });
+    //   // console.log("leasesModel1: ", this.leases);
+    //   // this.leases.forEach(lease => {
+    //   //   this.leasesModel = lease.customerLeasingForm;
+    //   //   console.log("leasesModel: ", this.leasesModel);
+    //   // })
+    // }
+
+  }
 }
