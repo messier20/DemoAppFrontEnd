635801c02f1f9b19d964b357096b2d5e9947f48b
monikossite <monikossite@gmail.com>
2018-04-08 00:50:34 +0300

    override colors using material theme variables

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -4,7 +4,7 @@ import {MediaMatcher} from "@angular/cdk/layout";
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
 
